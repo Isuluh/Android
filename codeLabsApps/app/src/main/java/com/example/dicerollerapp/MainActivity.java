87e2eecb1184d13efee1489f30ae9983cc7c46ad
hulusio <hulusi.ozkan@vestel.com.tr>
2019-12-30 16:11:52 +0300

    First app of CodeLabs is added

---
+++ codeLabsApps/app/src/main/java/com/example/dicerollerapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.dicerollerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button resetButton;
+    int randomNumber;
+    String textViewS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.show_count);
+
+
+    }
+
+    public void countUp(View view) {
+        if (textView.getText().toString().equals("A")) {
+
+            textView.setText("" + 1);
+            return;
+
+        } else {
+
+            int rollerInt = Integer.parseInt(textView.getText().toString());
+
+            if (rollerInt < 6) {
+                rollerInt++;
+                textView.setText("" + rollerInt);
+
+            }
+        }
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(MainActivity.this, "Merhaba", Toast.LENGTH_SHORT).show();
+
+        randomNumber = new Random().nextInt(6) + 1;
+        textView.setText("" + randomNumber);
+    }
+}
