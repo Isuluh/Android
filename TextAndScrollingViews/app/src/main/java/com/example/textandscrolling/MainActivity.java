f6b99b66ce9547bc3abcb3d7814184e39a75f393
hulusio <hulusi.ozkan@vestel.com.tr>
2020-01-03 11:44:07 +0300

    Version 01.3:Scrolling Text- Challenge

--- TextAndScrollingViews/app/src/main/java/com/example/textandscrolling/MainActivity.java
+++ TextAndScrollingViews/app/src/main/java/com/example/textandscrolling/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.textandscrolling;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+private Button addCommentButton;
+private TextView articleText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addCommentButton = (Button)findViewById(R.id.comment_button);
+        articleText =findViewById(R.id.articleText);
+        addCommentButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        articleText.append(" merhaba!!");
     }
 }
